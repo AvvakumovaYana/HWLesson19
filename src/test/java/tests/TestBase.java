d4982b03740ae726c67e8008d0e7d602761b90a3
Yana Avvakumova <10.07.93t@gmail.com>
2024-07-28 14:02:51 +0300

    Refactoring!

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,15 +1,10 @@
 package tests;
 
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.logevents.SelenideLogger;
 import config.ProjectConfiguration;
 import helpers.Attach;
-import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
-import java.util.Map;
 
 public class TestBase {
     @BeforeAll
